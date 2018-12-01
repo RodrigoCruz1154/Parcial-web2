4ba5ee8469584dcb522e18091ac7f609d619d04b
RodrigoCruz1154 <00115417@uca.edu.sv>
2018-12-01 16:36:51 -0600

    Controlador registrar

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -4,4 +4,18 @@ const cars = {};
 cars.mostrar = async function(req,res,next) {
     let cars = await car.find();
     return res.status(200).JSON(cars);
+};
+//Registra un nuevo carro
+cars.registrar = async function(req,res,next){
+    let car = new car();
+    car.marca = req.body.marca;
+    car.modelo = req.body.modelo;
+    car.annio = req.body.annio;
+
+    try {
+        await car.save();
+        return res.status(200).JSON({message:'Carro registrado con éxito'});
+    } catch (err) {
+       return res.status(500).JSON(err);
+    }
 }
