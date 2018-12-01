b20aae4b7292d77498c49f81d143a3fd15f5f8a7
RodrigoCruz1154 <00115417@uca.edu.sv>
2018-12-01 16:32:24 -0600

    Controlador mostrar

--- controllers/CarController.js
+++ controllers/CarController.js
@@ -1,20 +1,7 @@
 const car = require('../models/car');
 const cars = {};
-
-cars.showCars(async function(req,res,next){
-    let cars= await car.find();
+//Muestra todos los carros que hay
+cars.mostrar = async function(req,res,next) {
+    let cars = await car.find();
     return res.status(200).JSON(cars);
-});
-
-cars.save(async function(req,res,next){
-    let carro = new car();
-    carro.marca = req.body.marca;
-    carro.modelo = req.body.modelo;
-    carro.annio = req.body.annio;
-    try {
-        await save();
-        return res.status(200).JSON({message: 'Carro registrado'});
-    } catch (error) {
-        return res.status(500).JSON({message: 'Ha ocurrido un problema al momento de registrar su carro'});
-    }
-});
+}
